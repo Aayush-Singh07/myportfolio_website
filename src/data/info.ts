c8b687c435426e823febf475d3768c9f5ee5d488
Aayush-Singh07 <meeraaayush@gmail.com>
2024-08-26 17:40:16 +0530

    my-first-commit

---
+++ src/data/info.ts
@@ -0,0 +1,92 @@
+export const info = {
+  name: "Aayush Kumar Singh",
+  brief_description:
+    "I'm a mischievous full stack developer with a knack for troublemaking.",
+  role: "Full Stack Developer",
+  picture: "/pic.png",
+  picture_alt: "Bart Simpson",
+  location: "Vasco, ",
+  cv: "/resume.pdf",
+
+  about: {
+    description: `I'm a full stack developer with a passion for causing a little chaos in web development.
+    I have a solid foundation in computer science, although I prefer skateboarding over studying.
+    I excel in building web applications using React, Node.js, and MongoDB.
+    I'm known for my sharp wit and quick thinking, traits that serve me well in coding and pranks alike.
+    Looking for a challenge? Let's create something memorable together.`,
+    education: [
+      {
+        title: "Goa Engineering College",
+        date: "2023 - 2027",
+        location: "Farmagudi, Goa",
+        gpa: "8.5 ",
+        dept: "Information Techonology",
+      },
+    ],
+    experience: [
+      {
+        title: "Frontend Developer",
+        date: "2015 - Present",
+        location: "Krusty Burger Web Dev",
+        description:
+          "Crafted interactive web experiences using React and Redux. Often multitasked between coding and avoiding Principal Skinner.",
+      },
+      {
+        title: "Backend Bandit",
+        date: "2013 - 2015",
+        location: "Comic Book Guy's Software Emporium",
+        description:
+          "Masterminded backend solutions using Node.js and MongoDB. Surprisingly good at debugging, considering my history of causing bugs.",
+      },
+      {
+        title: "Junior Hacker",
+        date: "2010 - 2013",
+        location: "Springfield Elementary Cyber Club",
+        description:
+          "Pioneered early web exploits, leading the club in daring coding escapades. Was disciplined for hacking the school's grading system.",
+      },
+    ],
+
+    skills: ["JavaScript", "React", "Node.js", "MongoDB", "Prank Mastery"], // not used yet
+  },
+
+  projects: [
+    {
+      title: "Zomato Landing page",
+      date: "2024",
+      description:
+        "an interactive xomato landing page",
+      link: "https://github.com/bartsimpsonfake/prank-portfolio",
+      tech: ["React", "Node.js", "MongoDB", "Sarcasm.js"],
+      img_alt: "Bart's Portfolio",
+      img_path: "/simpsons.png",
+    },
+    {
+      title: "Skateboard Shop",
+      date: "2023",
+      description:
+        "An e-commerce site selling skateboards and prank supplies. No bullies allowed!",
+      link: "https://github.com/bartsimpsonfake/skate-shop",
+      tech: ["React", "Node.js", "MongoDB", "Skateboard.js"],
+      img_alt: "Skateboard Shop",
+      img_path: "skateboard.png",
+    },
+    {
+      title: "Comic Book Club",
+      date: "2022",
+      description:
+        "A platform for comic book enthusiasts to discuss and share their collections.",
+      link: "https://github.com/bartsimpsonfake/comic-club",
+      tech: ["React", "Node.js", "MongoDB", "Comic.js"],
+      img_alt: "Comic Book Club",
+      img_path: "comics.png",
+    },
+  ],
+
+  contact: {
+    email: "meeraaayush@gmail.com",
+    linkedin: "https://www.linkedin.com/in/aayushsingh",
+    github: "https://www.github.com/Aayush-Singh07",
+    twitter: "https://www.twitter.com/bartsimpsonfake",
+  },
+};
